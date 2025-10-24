09e9debb7f978e74882da14489677e561da9c6f4
Direwolf <rahulsood707@gmail.com>
2025-10-24 19:39:36 +0530

    added hooks

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,28 @@
 import axios from "axios"
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
+import type { Task } from "../types/task"
 
 const App = () => {
+  const [tasks, setTasks] = useState<Task[]>([])
+
   useEffect(() => {
-    axios.get("http://localhost:5228/api/tasks").then(console.log)
+    axios
+      .get<Task[]>("http://localhost:5228/api/tasks")
+      .then((res) => setTasks(res.data))
+      .catch((error) => console.error("Error fetching tasks:", error))
   }, [])
 
-  return <div>App</div>
+  return (
+    <div>
+      {tasks.map((task) => (
+        <div>
+          <span>{task.id}</span>
+          <span>{task.description}</span>
+          <span>{task.isCompleted}</span>
+        </div>
+      ))}
+    </div>
+  )
 }
 
 export default App
